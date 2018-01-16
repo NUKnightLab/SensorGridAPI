082cd77b3a7f58adc117c1de46b7f80d3d317577
Maxine Whitely <m.whitely16@gmail.com>
2018-01-16 10:55:43 -0600

    created model for db data

---
+++ sensorgridapi/sensorgridapi/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class SensorData(models.Model):
+    #check on this
+    battery = models.DecimalField(max_digits=3, demical_places=2, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    data1 = models.IntegerField(null=True)
+    data2 = models.IntegerField(null=True)
+    data3 = models.IntegerField(null=True)
+    message_id = models.IntegerField(null=True)
+    network = models.IntegerField(null=True)
+    node_id = models.IntegerField(null=True)
+    ram = models.IntegerField(null=True)
+    recieved_at = models.DateTimeField(auto_now_add=True)
+    record_id = models.CharField(max_length=100, blank=True, default='')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    version = models.IntegerField(null=True)
