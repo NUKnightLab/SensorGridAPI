cb1659205ec864d59fe35f6c18be6f72665a7d67
Alex Cohen <alexcohen711@gmail.com>
2018-01-22 20:22:21 -0600

    access to webpack, app.js makes call to API

--- sensorgriddashboard/src/APIFunctions/getJSONData.js
+++ sensorgriddashboard/src/APIFunctions/getJSONData.js
@@ -1,17 +1,17 @@
 
-import fetch from 'whatwg-fetch'
-
+import 'whatwg-fetch'
+// this function makes a call to the /data api and returns the JSON object
 export default function () {
   return new Promise((resolve, reject) => {
     fetch('http://54.152.62.254:9022/data')
       .then(function (response) {
-        console.log('check')
-        return resolve(response.json())
+        console.log(response)
+        return response.json()
       }).then(function (json) {
         console.log('parsed json', json)
+        resolve(json)
       }).catch(function (ex) {
         console.log('parsing failed', ex)
       })
   })
-
 }
--- sensorgriddashboard/src/App.js
+++ sensorgriddashboard/src/App.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import getJSON from './APIFunctions/getJSONData.js'
-
+//Currently this component is the whole application, it has a few boilerplate
+//things. When the component mounts it makes a call to the API and displays the first
+//battery datapoint
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ['test']
+    };
+  }
   componentWillMount() {
+    //this is called when the componenet mounts
+    //google component lifecycle for more info
     getJSON()
     .then( (res) => {
-      console.log('suh')
+      this.setState({
+        data: res.data
+      })
     })
   }
   render() {
+    //Render is the most important function of a react component
+    //This is where are describing what the component is displaying
+    var batteryLife = this.state.data[0].battery;
     return (
       <div className="App">
         <header className="App-header">
@@ -18,8 +33,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          {/*This takes the batteryLife variable and displays it  */}
+          {batteryLife}
+        </p>  
       </div>
     );
   }
