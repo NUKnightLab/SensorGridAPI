c47d5e33fb4421db5b600af310a0cb91ba1ba7f3
Alex Cohen <alexcohen711@gmail.com>
2018-01-30 11:08:56 -0600

    beginning implementation of graphs using semiotic

--- sensorgriddashboard/src/App.js
+++ sensorgriddashboard/src/App.js
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import getJSON from './APIFunctions/getJSONData.js'
 import BarChart from './BarChart'
+import { XYFrame, OrdinalFrame } from "semiotic"
+import testJSON from "./testData.json"
+
+
 //Currently this component is the whole application, it has a few boilerplate
 //things. When the component mounts it makes a call to the API and displays the first
 //battery datapoint
@@ -14,19 +18,45 @@ class App extends Component {
     };
   }
   componentWillMount() {
+    console.log(testJSON)
     //this is called when the componenet mounts
     //google component lifecycle for more info
-    getJSON()
-    .then( (res) => {
-      this.setState({
-        data: res.data
-      })
-    })
+    // getJSON()
+    // .then( (res) => {
+    //   this.setState({
+    //     data: res.data
+    //   })
+    // })
   }
   render() {
     //Render is the most important function of a react component
     //This is where are describing what the component is displaying
     var batteryLife = this.state.data[0].battery;
+    const data = [
+      { id: 'linedata-1', color: '#00a2ce', 
+         data: [ 
+              { sales: 500, daysSinceRelease: 1 }, 
+              { sales: 700, daysSinceRelease: 2 }, 
+              { sales: 0, daysSinceRelease: 3 }, 
+              { sales: 0, daysSinceRelease: 4 }, 
+              { sales: 200, daysSinceRelease: 5 }, 
+              { sales: 300, daysSinceRelease: 6 }, 
+              { sales: 500, daysSinceRelease: 7 } 
+             ] 
+      }
+  ]
+  const variousAnnotations = [
+    { orient: 'left', label: 'Battery Life' },
+    {orient: "bottom", label:"Time"}
+]
+const barChartData = [
+  { user: "Jason", tweets: 10, retweets: 5, favorites: 15 },
+  { user: "Susie", tweets: 5, retweets: 100, favorites: 100 },
+  { user: "Matt", tweets: 20, retweets: 25, favorites: 50 },
+  { user: "Betty", tweets: 30, retweets: 20, favorites: 10 }
+];      
+const test = testJSON.barTest
+console.log(test)
     return (
       <div className="App">
         <header className="App-header">
@@ -35,10 +65,30 @@ class App extends Component {
         </header>
         <p className="App-intro">
           {/*This takes the batteryLife variable and displays it  */}
-          {batteryLife}
         </p>
+        <XYFrame
+            size={[500,500]}
+            lines={testJSON}
+            xAccessor="test"
+            xExtent={ [0,7] }
+            yAccessor="battery"
+            yExtent={ [0,10] }
+            lineDataAccessor="lineTest"
+            showLinePoints = {true}
+            title={"Title of Graph"}
+            axes={variousAnnotations}
+            lineStyle={d => ({ stroke: 'red', fill: 'red' })}
+          />
+          <OrdinalFrame
+            size={[400, 600]}
+            data={test}
+            oAccessor={"sensor"}
+            rAccessor={"battery"}
+            style={{ fill: "#00a2ce", stroke: "white" }}
+            type={"bar"}
+            oLabel={true}
+          />
         <div>
-      <BarChart data={[5,10,1,3]} size={[500,500]} />
       </div>
       </div>
     );
