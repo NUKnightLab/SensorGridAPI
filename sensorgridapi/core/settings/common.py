e32e6120dc4504a902aed7ebe1b46fa8b6ccef42
michael_cahana <michaelcahana2018@u.northwestern.edu>
2018-02-06 10:55:27 -0600

    changing name of app folder from api to sensordata

--- sensorgridapi/core/settings/common.py
+++ sensorgridapi/core/settings/common.py
@@ -29,8 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api',
-    'requeststest.apps.RequeststestConfig'
+    'sensordata',
+    # 'requeststest.apps.RequeststestConfig'
     # 'sensordata.apps.SensorDataConfig'
 ]
 
