1ce47445d9a262a362384537f679b8554af4f210
Yingliao Wang <yingliaowang@dhcp-10-105-107-186.wireless.northwestern.private>
2018-02-22 11:26:15 -0600

    Summary and Map components done

--- sensorgriddashboard/src/components/HomePage.js
+++ sensorgriddashboard/src/components/HomePage.js
@@ -1,55 +1,71 @@
 import React, { Component } from 'react'
 import './HomePage.css'
+import NavBar from './NavBar'
+import Battery from './Battery'
+import MyGoogleMap from './MyGoogleMap'
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    Container,
-    Row,
-    Col,
-    Jumbotron,
-    Button
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+  Row,
+  Col,
+  Jumbotron,
+  Button,
+  ButtonGroup,
+  ButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
 } from 'reactstrap';
 
 class HomePage extends Component {
   constructor(props) {
-      super(props);
+    super(props);
 
-      this.toggle = this.toggle.bind(this);
-      this.state = {
-          isOpen: false
-      };
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
   }
   toggle() {
-      this.setState({
-          isOpen: !this.state.isOpen
-      });
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
   }
 
-render() {
-      return (
+  render() {
+    return (
+      <div>
+        <Navbar color="black" light expand="md">
+          <NavbarBrand href="/">SensorGrid Dashboard</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
         <div>
-          <Navbar color="black" light expand="md">
-            <NavbarBrand href="/">SensorGrid Dashboard</NavbarBrand>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
-              <Nav className="ml-auto" navbar>
-                <NavItem>
-                  <NavLink href="/components/">Components</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
-                </NavItem>
-                  </Nav>
-            </Collapse>
-          </Navbar>
+          <Battery className = "Battery" />
+          <NavBar className = "Navigation"/>
+          <MyGoogleMap />
+          
+
+
         </div>
-      )
-   }
+      </div>
+    )
+  }
 }
 
 export default HomePage
