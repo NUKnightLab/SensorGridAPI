3d9e7855b0c34f199c7abc9984c26c5d8730eff9
Scott Bradley <scottbradley.mail@gmail.com>
2018-03-01 10:48:53 -0600

    removes filter backend

--- sensorgridapi/core/settings/common.py
+++ sensorgridapi/core/settings/common.py
@@ -108,7 +108,7 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    #'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
